e2aa1354645c4f429023d17263e9ec00f8740fea
Alejandro Manjon - Cabeza Frances <amanjon-@c1r1s3.42madrid.com>
2023-12-04 16:49:56 +0100

    guardado main sin reestructuracion de philos zurdos cerrados

--- src/main.c
+++ src/main.c
@@ -98,7 +98,6 @@ void	ft_take_forks(t_philo *philo)
 
 void	ft_drop_forks(t_philo *philo)
 {
-	printf("suelto tenedores\n");
 	pthread_mutex_unlock(philo->right_fork);
 	pthread_mutex_unlock(philo->left_fork);
 	ft_print_status(philo, SLEEP);
@@ -107,16 +106,19 @@ void	ft_drop_forks(t_philo *philo)
 
 void	ft_philo_eat(t_philo *philo)
 {
-	pthread_mutex_unlock(philo->print_mutex);	// 1 -> entrada función
-	ft_take_forks(philo);
-	pthread_mutex_lock(philo->lock);
-	philo->last_meal = ft_get_time() - philo->data->start_time;
-	ft_print_status(philo, EAT);
-	pthread_mutex_unlock(philo->lock);
-	ft_usleep(philo->data->time_to_eat);
-	ft_drop_forks(philo);
-	ft_print_status(philo, THINK);
-	pthread_mutex_lock(philo->print_mutex);	// 2 -> salida función
+	while (philo->data->finish_program == 0)
+	{
+		pthread_mutex_unlock(philo->print_mutex);	// 1 -> entrada función
+		ft_take_forks(philo);
+		pthread_mutex_lock(philo->lock);
+		philo->last_meal = ft_get_time() - philo->data->start_time;
+		ft_print_status(philo, EAT);
+		pthread_mutex_unlock(philo->lock);
+		ft_usleep(philo->data->time_to_eat);
+		ft_drop_forks(philo);
+		ft_print_status(philo, THINK);
+		pthread_mutex_lock(philo->print_mutex);	// 2 -> salida función
+	}
 }
 
 void	*ft_routine(void *philo_struct)
@@ -152,12 +154,6 @@ void	ft_init_elems_and_create_threads(char **argv, t_data *data)
 		pthread_create(&(data->thread[i]), NULL, ft_routine, &(data->philos[i]));
 		i++;
 	}
-	i = 0;
-	// while (i < data->n_philos)
-	// {
-	// 	pthread_create(&(data->thread[i]), NULL, ft_routine, &(data->philos[i]));
-	// 	i++;
-	// }
 }
 
 int main(int argc, char **argv)
